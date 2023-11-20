5f04703b9fde17ab8f0b4af6e7b09558ee991fb9
ViliusAn <vilius.antanavicius123@gmail.com>
2023-11-20 11:02:33 +0200

    Init vue project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <div class="wrapper">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .wrapper {
+    background-color: var(--sidebar-background-color);
+  }
+</style>
