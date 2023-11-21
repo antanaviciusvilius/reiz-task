64b9a224ed4b1ebeadd659ac37ec3150335398a3
ViliusAn <vilius.antanavicius123@gmail.com>
2023-11-21 21:28:12 +0200

    Fetch helper tests

---
+++ src/__tests__/fetchHelper.spec.ts
@@ -0,0 +1,42 @@
+import fetchHelper, { type FetchRequestOptions } from '@/helpers/fetchHelper';
+import { expect, test } from 'vitest';
+
+// Mock fetch function
+global.fetch = async (url, _) => {
+  if (url === 'https://example.com/success') {
+    return {
+      ok: true,
+      json: async () => ({ success: true }),
+    } as Response;
+  }
+  return {
+    ok: false,
+    status: 404,
+    json: async () => ({ error: 'Not Found' }),
+  } as Response;
+};
+
+test('fetchHelper - successful GET request', async () => {
+  const requestOptions: FetchRequestOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  };
+
+  const response = await fetchHelper('https://example.com/success', requestOptions);
+  expect(response).toEqual({ success: true });
+});
+
+test('fetchHelper - failed GET request', async () => {
+  const requestOptions: FetchRequestOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  };
+
+  try {
+    await fetchHelper('https://example.com/fail', requestOptions);
+  } catch (error) {
+    if (error instanceof Error) {
+      expect(error.message).toBe('Request failed with status 404');
+    }
+  }
+});
